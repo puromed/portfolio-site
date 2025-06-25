7976a67905cdb297eb5bbdd96cecbb599c151719
puromed <arfanhaziq33@gmail.com>
2025-06-25 16:46:35 +0800

    Finally fix the download issue

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -330,7 +330,7 @@ function HomePage() {
 
             <motion.div variants={fadeIn} className="flex flex-wrap gap-4">
               <a
-                href='resume/Arfan_Razlan_Resume.pdf'
+                href='/resume/Arfan_Razlan_Resume.pdf'
                 download="Arfan_Razlan_Resume.pdf"
                 className={"inline-block bg-sky-500 hover:bg-sky-600 text-white font-bold py-3 px-6 rounded-lg " +
                   "transition duration-200 shadow-md hover:shadow-lg transform hover:scale-105 " +
